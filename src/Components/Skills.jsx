d92ef48d71db1a500c441392169b3373fb029fea
Yousuf Mohammadi <theehsansarshar@Yousufs-MacBook-Air.local>
2024-01-12 04:32:35 +0430

    skills component completed

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,166 @@
+import "../Styles/Components/Skills.css"
+export function Skills() {
+    return (
+      <div className="skills-container">
+        <p className="skill-par">
+          my <span>skills</span>
+        </p>
+        <div className="skills">
+          <div className="skill">
+            <img src="html.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">HTML</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="css.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">CSS</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="js.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">Js</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="bootstrap.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">Bootstrap</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="apollo.svg" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">apollo</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="mongodb.svg" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">mongodb</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="php.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">php</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="nodejs.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">nodejs</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="mysql.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">mysql</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="graphql.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">graphql</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="typescript.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">typescript</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="react.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">react</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="reactNative.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">reactNative</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+          <div className="skill">
+            <img src="github.png" alt="skill" className="skill-icon" />
+            <h2 className="skill-title">github</h2>
+            <div className="skill-percentage">
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="fill"></span>
+              <span className="empty"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Abstract } from "../Components/Abstract";
 import { Header } from "../Components/Header";
+import { Skills } from "../Components/Skills";
 
 export function Home() {
     return <div>
         <Header />
         <Abstract />
+        <Skills />
     </div>
 }
