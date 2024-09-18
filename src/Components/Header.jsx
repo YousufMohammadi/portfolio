909c8032fedfcea3c7f8640b97e5743697449536
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2024-09-18 14:52:46 +0330

    project image and links added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,41 +1,41 @@
 import { Link, useNavigate } from "react-router-dom";
-import "../Styles/Components/Header.css"
+import "../Styles/Components/Header.css";
 export function Header() {
-  const navigate = useNavigate()
-  function handleMenu() {
-    var x = document.getElementById("links");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  }
-    return (
-      <div className="header-container">
-        <div className="brand">Yousuf Mohammadi</div>
-        <div className="header-links" id="links">
-          <a className="link" href={"#home"}>
-            Home
-          </a>
-          <a className="link" href={"#about"}>
-            About
-          </a>
-          <a className="link" href={"#skills"}>
-            Skills
-          </a>
-          <a className="link" href={"#projects"}>
-            Projects
-          </a>
-          <a className="link" onClick={()=>navigate("/articles")}>
+	const navigate = useNavigate();
+	function handleMenu() {
+		var x = document.getElementById("links");
+		if (x.style.display === "flex") {
+			x.style.display = "none";
+		} else {
+			x.style.display = "flex";
+		}
+	}
+	return (
+		<div className="header-container">
+			<div className="brand">Yousuf Mohammadi</div>
+			<div className="header-links" id="links">
+				<a className="link" href={"#home"}>
+					Home
+				</a>
+				<a className="link" href={"#about"}>
+					About
+				</a>
+				<a className="link" href={"#skills"}>
+					Skills
+				</a>
+				<a className="link" href={"#projects"}>
+					Projects
+				</a>
+				{/* <a className="link" onClick={()=>navigate("/articles")}>
             Article
-          </a>
-          <a className="link" href={"#contact"}>
-            Contact
-          </a>
-        </div>
-        <Link onClick={handleMenu} id="menu">
-          <i class="fa-solid fa-bars"></i>
-        </Link>
-      </div>
-    );
+          </a> */}
+				<a className="link" href={"#contact"}>
+					Contact
+				</a>
+			</div>
+			<Link onClick={handleMenu} id="menu">
+				<i class="fa-solid fa-bars"></i>
+			</Link>
+		</div>
+	);
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,26 +1,42 @@
-import "../Styles/Components/Projects.css"
+import "../Styles/Components/Projects.css";
 export function Projects() {
-    return (
-      <div className="projects-container" id="projects">
-        <p className="section-heading">
-          MY <span>Projects</span>
-        </p>
-        <div className="projects">
-          <div className="project">
-            <img src="project1.png" alt="project" className="project-icon" />
-            <h2 className="project-heading">Oxygen DBMS</h2>
-            <a href="#" className="btn-active">
-              Demo <i class="fa-regular fa-eye download-icon"></i>
-            </a>
-          </div>
-          <div className="project">
-            <img src="project2.png" alt="project" className="project-icon" />
-            <h2 className="project-heading">gym power DBMS</h2>
-            <a href="#" className="btn-active">
-              Demo <i class="fa-regular fa-eye download-icon"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-    );
+	return (
+		<div className="projects-container" id="projects">
+			<p className="section-heading">
+				MY <span>Projects</span>
+			</p>
+			<div className="projects">
+				<div className="project">
+					<img src="project1.png" alt="project" className="project-icon" />
+					<h2 className="project-heading">Oxygen DBMS</h2>
+					<a
+						href="https://oxygen.techlands.online/"
+						target="_blank"
+						className="btn-active">
+						Demo <i class="fa-regular fa-eye download-icon"></i>
+					</a>
+				</div>
+				<div className="project">
+					<img src="project2.png" alt="project" className="project-icon" />
+					<h2 className="project-heading">gym power DBMS</h2>
+					<a
+						href="https://gym.techlands.online/"
+						target="_blank"
+						className="btn-active">
+						Demo <i class="fa-regular fa-eye download-icon"></i>
+					</a>
+				</div>
+				<div className="project">
+					<img src="project3.png" alt="project" className="project-icon" />
+					<h2 className="project-heading">Exchange Bills DBMS</h2>
+					<a
+						href="https://billing.techlands.online/"
+						target="_blank"
+						className="btn-active">
+						Demo <i class="fa-regular fa-eye download-icon"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
