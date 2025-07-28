d914e89601653fd2ac3c9e7e5da0968d9da829a6
YousufMOhammadi <yousufmohammadi3222@gmail.com>
2025-07-28 12:07:55 +0430

    new project added

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,7 +8,7 @@ export function Projects() {
 			<div className="projects">
 				<div className="project">
 					<img src="project1.png" alt="project" className="project-icon" />
-					<h2 className="project-heading">Oxygen DBMS</h2>
+					<h2 className="project-heading">Oxygen MIS</h2>
 					<a
 						href="https://oxygen.techlands.online/"
 						target="_blank"
@@ -18,7 +18,7 @@ export function Projects() {
 				</div>
 				<div className="project">
 					<img src="project2.png" alt="project" className="project-icon" />
-					<h2 className="project-heading">gym power DBMS</h2>
+					<h2 className="project-heading">gym power MIS</h2>
 					<a
 						href="https://gym.techlands.online/"
 						target="_blank"
@@ -28,7 +28,17 @@ export function Projects() {
 				</div>
 				<div className="project">
 					<img src="project3.png" alt="project" className="project-icon" />
-					<h2 className="project-heading">Exchange Bills DBMS</h2>
+					<h2 className="project-heading">Exchange Bills MIS</h2>
+					<a
+						href="https://billing.techlands.online/"
+						target="_blank"
+						className="btn-active">
+						Demo <i class="fa-regular fa-eye download-icon"></i>
+					</a>
+				</div>
+				<div className="project">
+					<img src="project4.jpg" alt="project" className="project-icon" />
+					<h2 className="project-heading">Parvaz MIS</h2>
 					<a
 						href="https://billing.techlands.online/"
 						target="_blank"
